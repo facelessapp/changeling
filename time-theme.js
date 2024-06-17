996c74539d0a31fc438f8e3668de20af57fb3e78
facelessapp <velhemax@yandex.ru>
2024-06-17 21:18:01 +0300

    Update time-theme.js

--- time-theme.js
+++ time-theme.js
@@ -1,9 +1,9 @@
 let timeRanges = new Set([
    {range: {from: `4`, to: `5`}, name: `sunrise`, href:`./sunrise.css`},
-   {range: {from: `5`, to: `10`}, name: `morning`, href:`./morning.css`},
-   {range: {from: `10`, to: `21`}, name: `day`, href:`./day.css`},
-   {range: {from: `21`, to: `23`}, name: `dawn`, href:`./dawn.css`},
-   {range: {from: `23`, to: `4`}, name:`night`, href:`./night.css`},
+   {range: {from: `6`, to: `10`}, name: `morning`, href:`./morning.css`},
+   {range: {from: `11`, to: `18`}, name: `day`, href:`./day.css`},
+   {range: {from: `19`, to: `23`}, name: `dawn`, href:`./dawn.css`},
+   {range: {from: `24`, to: `3`}, name:`night`, href:`./night.css`},
 ]);
 
 const changeTheme = timeRanges => {
